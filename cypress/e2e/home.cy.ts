f0f80db07c1a63a53a3467903c7e6028dc5339d0
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-18 19:09:22 -0400

    using dot only

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,8 +1,11 @@
 describe('home page', () => {
   it('the h1 contains the correct text', () => {
     cy.visit('http://localhost:3000')
-    cy.get("h1")
+    cy.get("[data-test='hero-heading']")
     .should("exist")
     .contains("Testing Next.js Applications with Cypress")
+  })
+  it.only("the features on the homepage are correct", ()=> {
+    cy.visit("http://localhost:3000")
   })
 })
