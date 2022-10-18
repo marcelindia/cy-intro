5514332b555f1503d9814c3ff1c5bd0fed5ecb73
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-18 19:00:48 -0400

    first commit

---
+++ cypress/e2e/home.cy.ts
@@ -0,0 +1,8 @@
+describe('home page', () => {
+  it('the h1 contains the correct text', () => {
+    cy.visit('http://localhost:3000')
+    cy.get("h1")
+    .should("exist")
+    .contains("Testing Next.js Applications with Cypress")
+  })
+})
