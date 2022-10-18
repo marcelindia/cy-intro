4307b3d4a4b8872c89501622a5657623750137b7
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-18 19:37:59 -0400

    test of non valid email && email that already exists

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -7,4 +7,15 @@ describe("Newsletter Subscribe Form", ()=> {
         cy.getByData("submit-button").click()
         cy.getByData("success-message").should("exist").contains("bob@aol.com")
     })
+    it('does NOT allow an invalid email address', ()=>{
+        cy.getByData("email-input").type("bob")
+        cy.getByData("submit-button").click()
+        cy.getByData("success-message").should("not.exist")
+    })
+    it("does NOT allow users to sign up TWICE", ()=> {
+        cy.getByData("email-input").type("john@example.com")
+        cy.getByData("submit-button").click()
+        cy.getByData("server-error-message").should("exist").contains("john@example.com")
+        
+    })
 })
