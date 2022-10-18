cfabb8c0e2eda18d0d6784a4478a96bb37e9bc27
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-18 19:15:55 -0400

    testing items in array && beforeEach

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,12 +1,21 @@
 describe('home page', () => {
-  it('the h1 contains the correct text', () => {
+  beforeEach(()=> {
     cy.visit('http://localhost:3000')
+  })
+  it('the h1 contains the correct text', () => {
+
     cy.get("[data-test='hero-heading']")
     .should("exist")
     .contains("Testing Next.js Applications with Cypress")
   })
+  // it.only("the features on the homepage are correct", ()=> {
+  //   cy.visit("http://localhost:3000")
+  //   cy.get("dt").eq(0).contains("4 Courses")
+  // it.only("the features on the homepage are correct", ()=> {
+  //   cy.visit("http://localhost:3000")
+  //   cy.get("dt").eq(1).contains("25+ Lessons")
   it.only("the features on the homepage are correct", ()=> {
-    cy.visit("http://localhost:3000")
-    cy.get("dt").eq(0)
+    
+    cy.get("dt").eq(2).contains("Free and Open Source")
   })
 })
