c8433544634fb1f61f8236d76eeb7b8acb87450e
Diana Marcelin <marcelindiana@yahoo.com>
2022-10-18 19:25:03 -0400

    created subscribe file, using getByData

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,8 @@
+describe("Newsletter Subscribe Form", ()=> {
+    beforeEach(()=> {
+        cy.visit('http://localhost:3000')
+    })
+    it("allows users to subscribe to the email list", ()=> {
+        cy.getByData("email-input")
+    })
+})
